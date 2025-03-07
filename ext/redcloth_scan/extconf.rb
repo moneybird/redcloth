7213f353b6b7ed1a9776a03165b54c83a25fddb9
Remco de Man <remcodeman@moneybird.com>
2025-03-07 14:56:24 +0100

    Support both amd64 and arm64

--- ext/redcloth_scan/extconf.rb
+++ ext/redcloth_scan/extconf.rb
@@ -1,4 +1,16 @@
 require 'mkmf'
+
+host_cpu = RbConfig::CONFIG['host_cpu']
+source_files = ['redcloth_attributes', 'redcloth_inline', 'redcloth_scan']
+
+source_files.each do |base|
+  if host_cpu =~ /arm64|aarch64/
+    File.rename("#{base}_arm64.c", "#{base}.c") if File.exist?("#{base}_arm64.c")
+  else
+    File.rename("#{base}_amd64.c", "#{base}.c") if File.exist?("#{base}_amd64.c") 
+  end
+end
+
 CONFIG['warnflags'].gsub!(/-Wshorten-64-to-32/, '') if CONFIG['warnflags']
 $CFLAGS << ' -O0 -Wall ' if CONFIG['CC'] =~ /gcc/
 dir_config("redcloth_scan")
